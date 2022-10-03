fdd7d34d0126774aa802f462f2c2ad1ca334c4b4
l1stochekkk <rubalkinmakx@gmail.com>
2022-10-03 22:09:34 +0300

    added 'material ui'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './styles/App.css'
 import './styles/navbar.css';
+import {Button} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="main-bg" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="main-bg"/>
+            <div className='absolute-util'>
+                <Button variant="outlined">Outlined</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
