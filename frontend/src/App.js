cb708f4fe1ef334428a5f44a9d7135990a2cdfe1
l1stochekkk <rubalkinmakx@gmail.com>
2022-10-03 22:37:18 +0300

    added 'material ui icons'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
+import * as React from 'react';
 import './styles/App.css'
 import './styles/navbar.css';
 import {Button} from "@mui/material";
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+function SimpleBottomNavigation() {
+    const [value, setValue] = React.useState(0);
+
+    return (
+        <Box sx={{ width: 150 }}>
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+            >
+                <BottomNavigationAction icon="" />
+                <BottomNavigationAction label="" />
+            </BottomNavigation>
+        </Box>
+    );
+}
 
 function App() {
+
     return (
         <div className="App">
             <div className="main-bg"/>
             <div className='absolute-util'>
+                <SimpleBottomNavigation/>
                 <Button variant="outlined">Outlined</Button>
             </div>
+
         </div>
     );
 }
